3b187e641033e5142556b9ab6ffdcc636e43bdd7
unknown <davidbigasgarcia@gmail.com>
2024-06-08 11:01:51 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <script setup>
-import { ref } from "vue";
-import contacts from "./contacts.json";
-const actors = ref([]);
+import { ref, watch } from "vue";
+import allContacts from "./contacts.json";
+const contacts = ref([]);
 
-// const addRandomContact = () => {
-//       const remainingContacts = contacts.filter(contact => !actors.value.includes(contact));
-//       const randomContact = remainingContacts[Math.floor(Math.random() * remainingContacts.length)];
-//       ators.vcalue.push(randomContact);
-//     };
 const addRandomContact = () => {
-  const remainingContacts = contacts.filter(contact => !actors.value.includes(contact));
+  const remainingContacts = allContacts.filter(contact => !contacts.value.includes(contact));
   const randomIndex = Math.floor(Math.random() * remainingContacts.length);
   const randomContact = remainingContacts[randomIndex];
-  actors.value.push(randomContact);
+  contacts.value.push(randomContact);
+};
+
+const sortElementsByName = () => {
+  contacts.value.sort((a, b) => a.name.localeCompare(b.name));
+};
+
+const sortElementsByPopularity = () => {
+  contacts.value.sort((a, b) => b.popularity - a.popularity);
+};
+
+const removeContact = (index) => {
+  contacts.value.splice(index, 1);
 };
 </script>
 
 <template>
   <div>
     <h1 class="title">Iron Contacts</h1>
-    <button @click="addRandomContact()">Add random contact</button>
+    <div class="button-container">
+    <button class="button" @click="addRandomContact">Add random contact</button>
+    <button class="button"  @click="sortElementsByName">Sort by name</button>
+    <button class="button" @click="sortElementsByPopularity">Sort by popularity</button>
+    </div>
     <table>
       <thead>
         <tr class="table-head-container">
-          <th>Picture</th>
+          <th class="picture">Picture</th>
           <th>Name</th>
           <th>Popularity</th>
           <th>Won Oscar</th>
@@ -31,12 +42,13 @@ const addRandomContact = () => {
         </tr>
       </thead>
       <tbody>
-        <tr class="table-data" v-for="(actor, index) in contacts.slice(0, 5)" :key="index">
-          <td><img :src="actor.pictureUrl" :alt="actor.name" width="100"></td>
+        <tr class="table-data" v-for="(actor, index) in contacts" :key="index">
+          <td><img :src="actor.pictureUrl" :alt="actor.name" width="100" /></td>
           <td>{{ actor.name }}</td>
           <td>{{ actor.popularity }}</td>
-          <td>{{ actor.wonOscar ? 'Yes' : 'No' }}</td>
-          <td>{{ actor.wonEmmy ? 'Yes' : 'No' }}</td>
+          <td>{{ actor.wonOscar ? "Yes" : "No" }}</td>
+          <td>{{ actor.wonEmmy ? "Yes" : "No" }}</td>
+          <button @click="removeContact" class="remove-button">Delete</button>
         </tr>
       </tbody>
     </table>
@@ -44,22 +56,44 @@ const addRandomContact = () => {
 </template>
 
 <style>
-.title{
+.title {
   text-align: center;
 }
-.table-head-container{
+.table-head-container {
   display: flex;
-
+  justify-content: space-evenly;
+  margin-right: 120px;
 }
-table{
- width: 100%;
+table {
+  width: 100%;
 }
-tr{
+tr {
   display: flex;
   justify-content: space-between;
 }
-.table-data{
+.table-data {
   margin-right: 30px;
   margin-top: 30px;
+}
+.button-container{
+  display: flex;
+  justify-content: space-around;
+  border: 10px, solid, black;
+  
+}
+.button{
+  background-color: black;
+  color: white;
+  margin-bottom: 60px;
+  margin-top: 40px;
+  
+}
+.button:hover{
+  cursor: pointer;
+  color:blue
+}
+.picture{
+  align-content: flex-start;
+  margin-left: 0;
 }
 </style>
